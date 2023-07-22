f0d945c5d4f00de1ace83bb7d9fbb6a5fe1fb243
ines <inesalves.1994@gmail.com>
2023-07-22 12:12:25 +0100

    Ines: 22/7 12:12

---
+++ SmartMarket/SmartMarket/Controllers/DescricaoUnidadeController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMarket.DTOs;
+using SmartMarket.Model;
+
+namespace SmartMarket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DescricaoUnidadeController : ControllerBase
+    {
+        private readonly SmartmarketdbContext _context;
+
+        public DescricaoUnidadeController(SmartmarketdbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<DescricaoUnidadeController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DescricaoUnidade>>> GetDescricaoUnidades()
+        {
+            return await _context.DescricaoUnidades.ToListAsync();
+        }
+
+        // GET api/<DescricaoUnidadeController>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DescricaoUnidade>> GetDescricaoUnidadeById(int id)
+        {
+            //trazer descricao igual ao do id passado
+            var descricaoUnidade = await _context.DescricaoUnidades.FindAsync(id);
+
+            if (descricaoUnidade == null)
+            {
+                return NotFound();
+            }
+
+            //retorna descricao do id
+            return descricaoUnidade;
+        }
+
+        //// POST api/<DEscricaoController>
+        [HttpPost]
+        public async Task<ActionResult<DescricaoUnidade>> CreateDescricaoUnidade(DescricaoUnidadeDto descricaoUnidade)
+        {
+            DescricaoUnidade descricaoUnidade1 = new DescricaoUnidade();
+            descricaoUnidade1 = descricaoUnidade.DtoToDescricaoUnidadeModel();
+            //adicionar marca passada por argumento
+            _context.DescricaoUnidades.Add(descricaoUnidade1);
+
+            //guardar marca
+            await _context.SaveChangesAsync();
+
+            return Ok(); //CreatedAtRoute(nameof(GetMarcaById), new { id = marca.IdMarca }, marca);
+        }
+
+        // PUT api/<MarcaController>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDescricaoUnidade(int id, DescricaoUnidade descricaoUnidade)
+        {
+            if (id != descricaoUnidade.IdDescricaoUnidade)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(descricaoUnidade).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DescricaoUnidadeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        // DELETE api/<MarcaController>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDescricaoUnidade(int id)
+        {
+            var family = await _context.DescricaoUnidades.FindAsync(id);
+
+            if (family == null)
+            {
+                return NotFound();
+            }
+
+            _context.DescricaoUnidades.Remove(family);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+        private bool DescricaoUnidadeExists(int id)
+        {
+            return _context.DescricaoUnidades.Any(m => m.IdDescricaoUnidade == id);
+        }
+    }
+}
